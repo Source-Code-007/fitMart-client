8b3ba7927ba5d5c0d8e5c96769f447171e839fc9
Source-Code-007 <utsho926@gmail.com>
2024-07-13 23:53:07 +0600

    added refresh warning and modified homepage

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,9 +1,48 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
 import CartCompo from "../components/cart/Cart";
+import { Modal } from "antd";
 
 const Cart = () => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const beforeUnloadRef = useRef();
+
+  const handleBeforeUnload = (event) => {
+    event.preventDefault(); // Standard for most browsers
+    event.returnValue = ""; // For some old browsers
+    setIsModalVisible(true); // Show the modal
+  };
+
+  useEffect(() => {
+    beforeUnloadRef.current = handleBeforeUnload;
+    window.addEventListener("beforeunload", beforeUnloadRef.current);
+
+    return () => {
+      window.removeEventListener("beforeunload", beforeUnloadRef.current);
+    };
+  }, []);
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+    window.removeEventListener("beforeunload", beforeUnloadRef.current);
+    window.location.reload(); // Allow page refresh or navigation
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
   return (
     <>
+      <Modal
+        title="Warning"
+        open={isModalVisible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        okText="Leave Page"
+        cancelText="Stay on Page"
+      >
+        <p>Are you sure you want to refresh? You will loss your cart data!</p>
+      </Modal>
       <CartCompo />
     </>
   );
